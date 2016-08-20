6d0b67a81c5aecef364c9be084f5de153ee32838
ANDRES SAENZ <SAENZA76@YAHOO.COM>
2016-08-20 16:18:49 -0500

    game is about done, not perfect

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,19 @@ function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function reset(){
+	function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	var randomNumber = getRandomInt(19, 120);
+	console.log(randomNumber);	
+
+	$('#randombox').text(randomNumber);
+
+	userNumber = 0;
+
+}
+
 var randomNumber = getRandomInt(19, 120);
 	console.log(randomNumber);
 
@@ -22,6 +35,78 @@ var sapphireNumber = getRandomInt(1, 12);
 
 $(document).ready(function(){
 	$('#randombox').text(randomNumber);
+
+
+	$('#diamond').click(function(){
+		userNumber = userNumber + diamondNumber;
+		console.log(userNumber);
+		$('#currentscore').text(userNumber);
+		if (userNumber === randomNumber){
+			alert("You Win!");
+			winCounter++;
+			$('#wins').text("Wins: " + winCounter);
+			reset()
+		}else if (userNumber > randomNumber){
+			alert("You Lose!");
+			lossCounter++;
+			$('#losses').text("Losses: " + lossCounter);
+			reset()
+		}
+	})
+
+	$('#emerald').click(function(){
+		userNumber = userNumber + emeraldNumber;
+		console.log(userNumber);
+		$('#currentscore').text(userNumber);
+		if (userNumber === randomNumber){
+			alert("You Win!");
+			winCounter++;
+			$('#wins').text("Wins: " + winCounter);
+			reset()
+		}else if (userNumber > randomNumber){
+			alert("You Lose!");
+			lossCounter++;
+			$('#losses').text("Losses: " + lossCounter);
+			reset()
+		}
+	})
+
+	$('#ruby').click(function(){
+		userNumber = userNumber + rubyNumber;
+		console.log(userNumber);
+		$('#currentscore').text(userNumber);
+		if (userNumber === randomNumber){
+			alert("You Win!");
+			winCounter++;
+			$('#wins').text("Wins: " + winCounter);
+			reset();
+		}else if (userNumber > randomNumber){
+			alert("You Lose!");
+			lossCounter++;
+			$('#losses').text("Losses: " + lossCounter);
+			reset()
+		}
+	})
+
+	$('#sapphire').click(function(){
+		userNumber = userNumber + sapphireNumber;
+		console.log(userNumber);
+		$('#currentscore').text(userNumber);
+		if (userNumber === randomNumber){
+			alert("You Win!");
+			winCounter++;
+			$('#wins').text("Wins: " + winCounter);
+			reset()
+		}else if (userNumber > randomNumber){
+			alert("You Lose!");
+			lossCounter++;
+			$('#losses').text("Losses: " + lossCounter);
+			reset()
+		}
+	})
+
+		$('#wins').text("Wins: " + winCounter);
+		$('#losses').text("Losses: " + lossCounter);
 
 
 })
